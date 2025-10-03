3b480b2bcce5d37359046018aec07e1c0bdda4d6
Moskinen <mortenlyngsie@gmail.com>
2025-10-03 11:39:37 +0200

    Ny controller der kan tage imod country databasen, som er tom lmao

--- src/main/java/com/example/countries/controllers/Pages.java
+++ src/main/java/com/example/countries/controllers/Pages.java
@@ -1,13 +1,19 @@
 package com.example.countries.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
-@RestController
+@Controller
 public class Pages {
 
     @GetMapping("/")
-    public String welcome(){
-        return "Welcome to the countries page";
+    public String home(){
+        return ("home/index");
     }
 }
+//@GetMapping("/")
+//public ModelAndView home(){
+//return new ModelAndView ("home/index")
